9e9f0c89b666812ef91d266fdd7cf6e918e2f325
Bagus1909 <bagussetiawan19092000@gmail.com>
2025-04-23 15:48:15 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+
+import TodoItem from "./components/TodoItem";
+
+type Todo = {
+  id: number;
+  title: string;
+};
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [input, setInput] = useState<string>("");
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleAddTodo = () => {
+    if (input.trim() === "") return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: input,
+    };
+
+    setTodos((todo) => [newTodo, ...todo]);
+
+    setInput("");
+  };
+
+  return (
+    <div className='bg-gray-500 h-screen w-screen flex flex-col items-center justify-center'>
+      <div className='h-[620px] w-[920px] flex flex-col bg-gray-200 p-4 rounded-lg shadow-lg'>
+        <h1 className='text-5xl mb-5 text-center'>Todo App</h1>
+        <div className='w-full flex gap-3'>
+          <input
+            type='text'
+            className='flex-1/2 p-2 border border-gray-300 rounded outline-none focus:border-blue-500'
+            placeholder='Add a new todo'
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleAddTodo();
+              }
+            }}
+            value={input}
+          />
+          <button
+            className='px-3 py-1 bg-sky-600 border-1 border-indigo-600 shadow-md shadow-indigo-600 rounded-md cursor-pointer'
+            onClick={handleAddTodo}
+          >
+            Add
+          </button>
+        </div>
+
+        <ul>
+          {todos.map((todos) => (
+            <TodoItem
+              key={todos.id}
+              todos={todos}
+              setTodos={setTodos}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+type Todo = {
+  id: number;
+  title: string;
+};
+
+type TodoItemProps = {
+  todos: {
+    id: number;
+    title: string;
+  };
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const TodoItem: React.FC<TodoItemProps> = ({ todos, setTodos }) => {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  return (
+    <li
+      key={todos.id}
+      className='p-2 my-2 rounded shadow-md flex justify-between items-center'
+    >
+      {isEditing ? (
+        <input
+          value={todos.title}
+          className='flex-1/2 p-2 border border-gray-300 rounded outline-none focus:border-blue-500 me-2'
+          onChange={(e) => {
+            setTodos((prev) => prev.map((todo) => (todo.id === todos.id ? { ...todo, title: e.target.value } : todo)));
+          }}
+        />
+      ) : (
+        <span className=''>{todos.title}</span>
+      )}
+      <div className='flex gap-1'>
+        {isEditing ? (
+          <button
+            className='px-3 py-1 bg-green-500 border-1 border-indigo-600 shadow-md shadow-indigo-600 rounded-md cursor-pointer'
+            onClick={() => {
+              setIsEditing(false);
+            }}
+          >
+            Update
+          </button>
+        ) : (
+          <button
+            className='px-3 py-1 bg-green-500 border-1 border-indigo-600 shadow-md shadow-indigo-600 rounded-md cursor-pointer'
+            onClick={() => setIsEditing(true)}
+          >
+            Edit
+          </button>
+        )}
+        <button
+          className='px-3 py-1 bg-red-500 border-1 border-indigo-600 shadow-md shadow-indigo-600 rounded-md cursor-pointer'
+          onClick={() => {
+            setTodos((prev) => prev.filter((todo) => todo.id !== todos.id));
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
